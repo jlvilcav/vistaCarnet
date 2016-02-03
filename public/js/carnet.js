df6c8b891976e380ece8a71f739c5b94cfafbb81
Jorge Vilca <jlvilcavgmail.com>
2016-02-03 11:20:30 -0500

    Avance en carnet.js, se creo funcion para el click del boton buscar.

--- public/js/carnet.js
+++ public/js/carnet.js
@@ -1,1 +1,12 @@
 // Documento Java Script
+
+var dominio = document.domain;
+var urlBase = "http://"+ dominio + "/reclamaciones/public/";
+
+$(document).on('ready',function(){
+
+	$("#vBtnBuscar").on('click', function(){
+		
+	});
+
+});
