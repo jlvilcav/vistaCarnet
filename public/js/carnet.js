a2bc67be8c203f989481570b44029394fb220d0c
Jorge Vilca <jlvilcavgmail.com>
2016-02-04 09:10:07 -0500

    Se creo funcion buscarCarnetAlumno en  carnet.js

--- public/js/carnet.js
+++ public/js/carnet.js
@@ -6,7 +6,20 @@ var urlBase = "http://"+ dominio + "/reclamaciones/public/";
 $(document).on('ready',function(){
 
 	$("#vBtnBuscar").on('click', function(){
-		
+		var buscar = new buscarCarnetAlumno();
+		buscar.buscarxCarnet();
 	});
 
 });
+
+function buscarCarnetAlumno(){
+	var $el =$("<div>");
+	$el.url = urlBase + 'carnet/lista/';
+
+	$el.on('noseEncontro', function(){
+		alert('No se ha emitido carnet para el código ingresado');
+		$('#vTxtCodigo').focus();
+	});
+
+
+}
